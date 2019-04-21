92d28d17f1fe1a3a7494d3db67272eb51172f8e4
Fluegel, Eldiar <Eldiar.Fluegel.hmb@fh-zwickau.de>
2019-04-22 00:02:00 +0200

    Save Button implemented for saving changes in the Database

--- src/application/view/MainController.java
+++ src/application/view/MainController.java
@@ -1,26 +1,24 @@
 package application.view;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
-import java.util.function.Supplier;
 
 import application.TableInfo.TableColumnData;
 import application.TableInfo.TableData;
 import application.model.BaseEntity;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
@@ -30,8 +28,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.util.StringConverter;
 
 public class MainController
@@ -41,8 +37,7 @@ public class MainController
 	private TabPane tabView;
 
 	private Map<String, TableData> tableData = new TreeMap<String, TableData>();
-	String connectionUrl = "jdbc:sqlserver://localhost:1433;" + "database=PZM1;" + "user=SQLTEACHER;"
-			+ "password=12345;";
+	String connectionUrl = "jdbc:sqlserver://DESKTOP-K7FKTFD\\\\SQLEXPRESS:61180;" + "database=PZM1;" + "user=Eldiar;" + "password=12345;";
 
 	@FXML
 	public void initialize()
@@ -56,9 +51,7 @@ public class MainController
 
 			while (resultSet.next())
 			{
-				if ("trace_xe_action_map".equals(resultSet.getString(3))
-						|| "trace_xe_event_map".equals(resultSet.getString(3))
-						|| "sysdiagrams".equals(resultSet.getString(3)))
+				if ("trace_xe_action_map".equals(resultSet.getString(3)) || "trace_xe_event_map".equals(resultSet.getString(3)) || "sysdiagrams".equals(resultSet.getString(3)))
 					continue;
 				ResultSet foreignKeys = meta.getImportedKeys(null, null, resultSet.getString(3));
 				addNewTable(resultSet.getString(3), foreignKeys);
@@ -70,8 +63,7 @@ public class MainController
 			e.printStackTrace();
 		}
 
-		tableData.forEach((key, value) ->
-		{
+		tableData.forEach((key, value) -> {
 			addTab(value);
 		});
 		tabView.tabMinWidthProperty().set(100);// set the tabPane's tabs min and max widths to be the same.
@@ -82,6 +74,11 @@ public class MainController
 		// the tabs combined width + a padding value
 	}
 
+	/**
+	 * Create and add a Tab to the TabView
+	 * 
+	 * @param data
+	 */
 	public void addTab(TableData data)
 	{
 
@@ -109,14 +106,16 @@ public class MainController
 				ComboBox<BaseEntity> combo = new ComboBox();
 				ObservableList<BaseEntity> l = tableData.get(data.getForeignKeys().get(entry.getKey())).getValues();
 				combo.setItems(l);
-				combo.setConverter(new StringConverter<BaseEntity>() {
+				combo.setConverter(new StringConverter<BaseEntity>()
+				{
 					@Override
 					public String toString(BaseEntity object)
 					{
-						if(object == null)
+						if (object == null)
 						{
 							return "NULL";
 						}
+						// return object.getValue(enty.getKey());
 						return object.getDbEntryValueMap().toString();
 					}
 
@@ -129,24 +128,22 @@ public class MainController
 					}
 				});
 				grid.add(combo, 1, i++);
-				
-				data.getSelectedEntity().addListener((observableVal, old, newValue) ->
-				{
+
+				data.getSelectedEntity().addListener((observableVal, old, newValue) -> {
 					if (newValue != null)
 					{
-						
+
 						TableColumnData foreign = entry.getValue();
 						System.out.println(foreign.getForeignTable());
-						BaseEntity entity = tableData.get(foreign.getForeignTable()).getValues().stream().filter(ent ->
-						{
-							if(newValue.getValue(entry.getKey()).equals(ent.getValue(foreign.getForeignColumn()))) return true;
+						BaseEntity entity = tableData.get(foreign.getForeignTable()).getValues().stream().filter(ent -> {
+							if (newValue.getValue(entry.getKey()).equals(ent.getValue(foreign.getForeignColumn())))
+								return true;
 							return false;
 						}).findFirst().get();
 						combo.getSelectionModel().select(entity);
 					}
 				});
-				
-				
+
 			} else
 			{
 				Label label = new Label(entry.getKey());
@@ -156,8 +153,7 @@ public class MainController
 				grid.add(text, 1, i++);
 
 				// Listener for selecting an a new entity
-				data.getSelectedEntity().addListener((observableVal, old, newValue) ->
-				{
+				data.getSelectedEntity().addListener((observableVal, old, newValue) -> {
 					if (newValue != null)
 					{
 						text.setText(newValue.getValue(entry.getKey()));
@@ -165,9 +161,8 @@ public class MainController
 				});
 
 				// Listener for changing the entity based on values
-				text.textProperty().addListener((observable, oldValue, newValue) ->
-				{
-					if (data.getSelectedEntity() != null)
+				text.textProperty().addListener((observable, oldValue, newValue) -> {
+					if (data.getSelectedEntity().get() != null)
 					{
 						data.getSelectedEntity().getValue().setValue(entry.getKey(), newValue);
 					}
@@ -176,16 +171,18 @@ public class MainController
 
 			// Create TableColumn
 			TableColumn<BaseEntity, String> column = new TableColumn(entry.getKey());
-			column.setCellValueFactory(
-					cellValue -> new SimpleStringProperty(cellValue.getValue().getValue(entry.getKey())));
+			column.setCellValueFactory(cellValue -> new SimpleStringProperty(cellValue.getValue().getValue(entry.getKey())));
 			table.getColumns().add(column);
 		}
 		// Set Listener on table for selecting an entity
-		table.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) ->
-		{
+		table.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
 			data.setSelectedEntity(newSelection);
 		});
 		grid.add(table, 0, i, 5, 1);
+
+		// SAVE BUTTON
+		addSaveButton(data, grid, i);
+
 		mitTab.setContent(grid);
 		table.setItems(data.getValues());
 	}
@@ -211,6 +208,61 @@ public class MainController
 		{
 			e.printStackTrace();
 		}
+	}
+
+	public void addSaveButton(TableData data, GridPane grid, int i)
+	{
+		Button saveButton = new Button();
+
+		saveButton.setText("Save Changes");
+		grid.add(saveButton, 0, i + 1);
+
+		saveButton.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent e)
+			{
+				try
+				{
+					if (data.getSelectedEntity().get() != null)
+					{
+						setConnection(DriverManager.getConnection(connectionUrl));
+
+						Statement statement = getConnection().createStatement();
+						String dataSet = data.getSelectedEntity().get().getDbEntryValueMap().toString().replace("{", "").replace("}", "");
+
+						String QUERY = "";
+						// Temporary!!! TODO
+						String id = "";
+						String idColumnName = "";
+
+						for (Entry<String, String> entry : data.getSelectedEntity().get().getDbEntryValueMap().entrySet())
+						{
+							if (entry.getKey().contains("Nr"))
+							{
+								id = entry.getValue();
+								idColumnName = entry.getKey();
+							}
+//						System.out.println("Main Controler data.getSelectedEntity Entries: " + entry.getValue());
+							String entryToUpdate = entry.getKey() + "= '" + entry.getValue() + "'";
+//						System.out.println("Main Controller Entry To Update: " + entryToUpdate);
+							String entryQUERY = "UPDATE " + data.getTableName() + " SET " + entryToUpdate + " WHERE " + idColumnName + " ='" + id + "'; \n";
+//						System.out.println("Main Controller QUERY: " + entryQUERY);
+
+							QUERY += entryQUERY;
+
+						}
+						System.out.println("WHOLE UPDATE QUERY: " + QUERY);
+						statement.execute(QUERY);
+					}
+
+				} catch (SQLException ex)
+				{
+					ex.printStackTrace();
+				}
+			}
+		});
+
 	}
 
 	public Connection getConnection()
